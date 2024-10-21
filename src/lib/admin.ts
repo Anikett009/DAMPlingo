eb26de4734cfcd152f38d542d10864d32cc0a5a4
Anikett009 <aniketpethe007@gmail.com>
2024-10-21 15:16:29 +0530

    added admin

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,6 +1,6 @@
 import { auth } from "@clerk/nextjs";
 
-const adminIds = ["user_2lL9XWBa8l7ZB0Cj15NGF2Us6lu"];
+const adminIds = ["user_2lL9XWBa8l7ZB0Cj15NGF2Us6lu", "user_2nk14kE9uL8ZBtr9dIOPXFy8ERx","user_2lgDGQkEdpQqTKqeab0GIROTszJ","user_2nk3588RaIroi1m8zpIX9x7lOv0"];
 
 export const isAdmin = () => {
   const { userId } = auth();
